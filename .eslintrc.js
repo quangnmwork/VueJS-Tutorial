241eb7b61b81b065e3dc305270115e3b4766923b
Quang Nguyen <quangnm.work@gmail.com>
2022-03-21 20:59:23 +0700

    add template syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     quotes: ['error', 'single', { avoidEscape: true }],
+    'space-before-function-paren': ['error', 'never'],
     semi: ['error', 'always', { omitLastInOneLineBlock: true }]
   }
 };
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <button class="btn-app" v-bind="objectAtributes">Click here</button>
   <HelloWorld msg="<span style='color:red'>Hello world</span>" />
+  <p v-bind:title="toTitle()">Functioncall</p>
+  <button @click="showAlert()">Dont touch me</button>
+  <form @submit.prevent="showInfo(isShowMessage)"><button>Submit</button></form>
 </template>
 
 <script>
@@ -10,6 +14,23 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      isShowMessage: true,
+      objectAtributes: { id: 'btn-app', disabled: true },
+      toTitle() {
+        return 'hello';
+      }
+    };
+  },
+  methods: {
+    showAlert: () => {
+      alert('Don touch me');
+    },
+    showInfo: (isShowMessage) => {
+      alert(isShowMessage);
+    }
   }
 };
 </script>
