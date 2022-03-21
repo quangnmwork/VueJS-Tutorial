96a00e3ac1d18c161314fceb9f0e94dc0008a86a
Quang Nguyen <quangnm.work@gmail.com>
2022-03-21 20:10:23 +0700

    add rawHTML

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="<span style='color:red'>Hello world</span>" />
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="hello" id="hello">
+    <h1>{{ msg }} My god</h1>
+    <span v-html="msg"></span>
   </div>
 </template>
 
