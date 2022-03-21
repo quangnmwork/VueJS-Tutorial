981752084aab8d438f5ab3f74ac11941b0d3d2e9
Quang Nguyen <quangnm.work@gmail.com>
2022-03-21 21:15:57 +0700

    init reactivity

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'space-before-function-paren': ['error', 'never'],
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }]
   }
-}
+};
