6b88e303f2d42ac887f38e6a1ef70c80d543403b
Quang Nguyen <quangnm.work@gmail.com>
2022-03-22 20:12:06 +0700

    add list rendering

--- lifecycle-vue/src/components/ConditionComponent.vue
+++ lifecycle-vue/src/components/ConditionComponent.vue
@@ -6,6 +6,12 @@
     <p v-else>Yes you are</p>
     <p v-show="number > 20">Hi Im your uncle</p>
     <button @click="randomAge()">Random your age</button>
+    <p>History</p>
+    <ul>
+      <li v-for="(eleHistory, index) in history" :key="index">
+        <p :class="eleHistory > 20 ? 'isOld' : 'isYoung'">{{ eleHistory }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 <script lang="ts">
@@ -14,15 +20,24 @@ export default defineComponent({
   name: 'ConditionComponent',
   data() {
     return {
-      number: 20 as Number
+      number: 20 as Number,
+      history: [] as Number[]
     };
   },
   methods: {
     randomAge() {
       const randomNumber = Math.floor(Math.random() * 100);
       this.number = randomNumber;
+      this.history.push(this.number);
     }
   }
 });
 </script>
-<style scoped></style>
+<style scoped>
+.isOld {
+  color: brown;
+}
+.isYoung {
+  color: aqua;
+}
+</style>
