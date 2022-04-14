6ab01b5273ae008e13bf8eabc4b1311d2e37d37f
Quang Nguyen <quangnm.work@gmail.com>
2022-04-15 00:30:18 +0700

    adding query

---
+++ composition/src/components/QueryBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <input
+    type="text"
+    :value="props.query"
+    @input="$emits('change', $event.target.value)"
+  />
+</template>
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+const props =
+  defineProps<{ query: string }>(); /* eslint-disable func-call-spacing */
+const emits = defineEmits<{ (event: 'change', query: string): void }>();
+</script>
