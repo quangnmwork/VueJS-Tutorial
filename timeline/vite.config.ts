82f2550d12725a7301b4f1338062b4a7684f89a4
Quang Nguyen <quangnm.work@gmail.com>
2022-07-26 16:54:52 +0700

    chore: add uno-css

--- timeline/vite.config.ts
+++ timeline/vite.config.ts
@@ -1,11 +1,21 @@
 import { fileURLToPath, URL } from "node:url";
-
+import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
-
+import UnoCSS from "unocss/vite";
+import { presetUno, presetAttributify, presetIcons } from "unocss";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components()],
+  plugins: [
+    vue(),
+    Components(),
+    AutoImport({
+      /* options */
+    }),
+    UnoCSS({
+      presets: [presetUno(), presetAttributify(), presetIcons()],
+    }),
+  ],
 });
 
