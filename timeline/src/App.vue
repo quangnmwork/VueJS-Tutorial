1bbfa9d35d7d822dceadb28fbcc7a4f0a2e4566d
Quang Nguyen <quangnm.work@gmail.com>
2022-07-26 16:14:50 +0700

    chore: init components

--- timeline/src/App.vue
+++ timeline/src/App.vue
@@ -1,21 +1,5 @@
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Hello name="Quang" />
 </template>
 
 <style>
---
+++ timeline/src/components/Hello.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+});
+</script>
+<template>
+  <h1>Hello {{ name }}</h1>
+</template>
